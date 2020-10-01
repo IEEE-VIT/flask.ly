64ce219baa002b07bea55e28fb67121a22f235bf
Andrew Miko <andrew.miko@yandex.com>
2020-10-01 16:50:24 +0300

    Updated app file according to flask sqlalchemy requirements

--- app.py
+++ app.py
@@ -1,9 +1,21 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 from routes.routes import router
 
-app=Flask(__name__)
 
-app.register_blueprint(router);
+app = Flask(__name__)
+app.config.from_object('settings')
+app.register_blueprint(router)
+db = SQLAlchemy(app)
 
-if __name__=='__main__':
+
+def db_init():
+    from models import Url, User
+    db.create_all()
+    db.session.commit()
+    db.session.close()
+
+
+if __name__ == '__main__':
+    db_init()
     app.run()
