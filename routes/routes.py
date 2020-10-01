95790be4d531724526c581d560dd2fd6a4f132a6
coldkillerr <yonishitjain123456@gmail.com>
2020-10-01 19:50:53 +0530

    first commit

--- routes/routes.py
+++ routes/routes.py
@@ -1,16 +1,9 @@
-from base64 import encode, encodebytes
 from flask import Blueprint
-<<<<<<< HEAD
 from flask import render_template,url_for,flash,redirect
 from  werkzeug.security import generate_password_hash , check_password_hash
 from forms import RegistrationForm
+from models import db ,User
 
-=======
-import base64
-import time
-from flask.globals import request
-from werkzeug.utils import redirect
->>>>>>> 0f3c64a82d9cbf73614c1e846c21f13bbbe674e1
 router = Blueprint("router", __name__)
 
 key_url_mapping={}
@@ -22,27 +15,12 @@ key_url_mapping={}
 def status():
     return "Up and Running ! :)"
 
-@router.route('/api/shorten/quick', methods=['POST'])
+@router.route('/api/shorten/quick')
 def quickShorten():
-    global key_url_mapping
-    try:
-        url = request.json
-        t = str(time.time())
-        encoded_url = base64.urlsafe_b64encode(url['url'].encode())
-        print(encoded_url)
-        encoded_url = (encoded_url[0:3]+encoded_url[-5:-2]).decode('utf-8')
-        if(encoded_url not in key_url_mapping):
-            key_url_mapping[encoded_url] = url['url']
-        else:
-            if(url['url'] != key_url_mapping[encoded_url]):
-                encoded_url = base64.urlsafe_b64encode((url['url']+t).encode())
-                encoded_url = (encoded_url[0:3]+encoded_url[-5:-2]).decode('utf-8')
-                key_url_mapping[encoded_url] = url['url']
-        return "http://127.0.0.1:5000/{}".format(encoded_url)
-    except Exception as e:
-        print(e)
-        return "Error Occured. Try Later"
-    # return 1;
+    # Create a dictionary which has the shortened URL as the key
+    # And the actual URL as the value
+    # For easy implementation, just keep auto incrementing the key 
+    return 1;
 
 @router.route('/api/shorten/url')
 def shorten():
@@ -65,23 +43,21 @@ def register():
     if form.validate_on_submit():
     	hashed_password = generate_password_hash(form.password.data , method='sha256')
     	existing_users = User.query.all()
+     
     	if existing_users:
-    		for user in existing_users:
+    		for user in existing_users :
                 if form.email.data == user.email_id :
                     flash('Email already taken . Please try again .','danger')
                     return redirect(url_for('register'))
- 			
-
-    			
-		
-		new_user = User(email_id = form.email.data  , password = hashed_password)
-	# 	db.session.add(new_user)
-		
-		
-	# 	db.session.commit()
-	# 	flash('Registration Successfull','success')
-	# 	return redirect(url_for('login'))
-	# return render_template('Registrationform.html',title='register',form=form)
+        
+        
+ 		new_user = Lawyer( email_id = form.email.data  , password = hashed_password)
+		db.session.add(new_user)
+		db.session.commit()
+        flash('Registration Successfull','success')
+        return redirect(url_for('customShorten'))
+	# return render_template('Registrationform.html',form=form) 
+    # Uncomment this when you add a registration page template
     return 1;
 
 @router.route('/api/login')
@@ -108,14 +84,9 @@ def customShorten():
     # Else ask the user to enter any other custom URL and repeat the process
     return 1;
 
-@router.route('/<key>', methods=['GET'])
-def redirectToActualURL(key):
-    global key_url_mapping
-    build_url = "https://"+key_url_mapping[key] if key_url_mapping[key][:4] != "http" else key_url_mapping[key]
-    if(key in key_url_mapping):
-        return redirect(build_url)
-    else:
-        return "Not Found"
+@router.route('/')
+def redirectToActualURL():
     # Get the entered URL as (localhost:5000/shortenedURL)
     # Now, this route finds the actual URL corresponding to the given one
     # and redirects the user to that actual URL
+    return 1;
